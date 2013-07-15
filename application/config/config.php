958448b22b1ebf32bcbbe5efa6cfb8480f924655
maple redwing <redmaplewing@gmail.com>
2013-07-15 21:57:31 +0800

    Revert "Like all application create by codeIgniter"
    This reverts commit 4df0ccbab7e743eb4d51101b67b7df53cdbf9bcc.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://mapleWebERP.localhost/';
+$config['base_url']	= 'http://unisun.localhost/';
 
 /*
 |--------------------------------------------------------------------------
