167869903eee06074715a304c79a174336186a5d
maple redwing <redmaplewing@gmail.com>
2013-07-16 00:23:41 +0800

    再次修改 application/config/config.php的參數 base_url=>mapleWebERP.localhost

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://unisun.localhost/';
+$config['base_url']	= 'http://mapleWebERP.localhost/';
 
 /*
 |--------------------------------------------------------------------------
